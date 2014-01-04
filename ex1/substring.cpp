3ae06f43b82d97070b4982c5f8d7048044aeac04
alex <alex@adl.(none)>
2014-01-04 12:13:06 +0000

    substring done, non recursive

--- ex1/substring.cpp
+++ ex1/substring.cpp
@@ -1,8 +1,30 @@
 #include<iostream>
+#include<cstring>
 
 using namespace std;
 
-int substring_position(const char* substring, const char* string) {
+int substring_position(const char* substring, const char* target) {
+  char *pt, string[100];
+  int length;
+  strcpy(string, target);
 
+  for(length=0; substring[length]!='\0'; length++);
+
+  //  cerr << "length is " << length << endl;
+
+  if(substring[0]=='\0' && string[0]=='\0')
+    return 0;
+
+  for(int i=0; string[i]!='\0'; i++) {
+    pt = &string[i];
+
+    //    cerr << "about to compare '" << substring << "' with '" << pt << "'" << endl;
+
+    if (strncmp(substring, pt, length)==0)
+      return i;
+  }
+
+  return -1;
 }
+
 
