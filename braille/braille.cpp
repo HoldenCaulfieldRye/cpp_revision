9173a922aa3bcce38e26afd131882382fd5b1895
HoldenCaulfieldICL <adalyac@gmail.com>
2014-01-07 18:47:38 +0000

    try with transpose

--- braille/braille.cpp
+++ braille/braille.cpp
@@ -139,7 +139,7 @@ void encode(const char* str, char* braille) {
 
 
 void print_braille(const char* str, ostream& out) {
-  char braille[100], matrix[4][600];
+  char braille[100], matrix[600][4];
 
   //create encoded text
   encode(str, braille);
@@ -151,8 +151,8 @@ void print_braille(const char* str, ostream& out) {
 
 
   //print matrix to out
-  for (int row=0; row<4; row++) {
-    for (int col=0; matrix[0][col]!='\0'; col++)
+  for (int col=0; matrix[0][col]!='\0'; col++) {
+    for (int row=0; row<4; row++)
       out << matrix[row][col];
     out << endl;
   }
@@ -190,12 +190,13 @@ void matrify(const char* str, char* braille, char m[][600]) {
     cerr << endl << "m[3][3] = " << str[0] << endl;
 
     m[3][3] = str[0];
-
+    return matrify(&str[1], &braille[12], &m[][6]); 
     /*    return matrify(&str[1], &braille[12], &m[6]); 
 &m[6] is wrong, because in matrify() declaration, you say your 3rd param is m[][600], an array of 600-character rows. but with &m[6] you are calling the 6th row, and you have no guarantee that it exists. 
 in fact, you're only ever writing data to the first 3 rows!
-what you really want to call, in this specific case, is &m[][6], ie the 6th column in your current argument. 
-and I think your parameter needs to change to m[4][], which basically forces the compiler to ensure that you will never run out of columns.
+what you really want to call, in this specific case, is &m[][6], ie the address of the 6th column in your current argument. But you can't do that; you can only call the address of the k-th entry of your array, in this case the k-th row of your matrix.
+C++ won't let you have m[4][] as a parameter.
+The solution is to have m[][4] as a parameter, and print the transpose of this function.
      */
   }
 
@@ -223,6 +224,6 @@ and I think your parameter needs to change to m[4][], which basically forces the
 
     cerr << "neither m[3][0] nor str[0] are segfaulting" << endl;
 
-    return matrify(&str[1], &braille[6], &m[3]);      
+    return matrify(&str[1], &braille[6], &m[][3]);      
   }
 }
