1f80bf31a206f9a385e3fb115293fd2b9ec821d8
HoldenCaulfieldICL <adalyac@gmail.com>
2014-01-08 18:08:17 +0000

    working on piglatin

---
+++ piglatin/piglatin.h
@@ -0,0 +1,7 @@
+int findFirstVowel(const char* word);
+
+void translateWord(const char* word, char* translated);
+
+void translateStream(istream& input, ostream& cout);
+
+/* void translateStream(istream input, ostream output); */
