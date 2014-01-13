b6328ed09ba2dedb7f7e3538cc9927da30e9743d
alex <adalyac@gmail.com>
2014-01-13 13:27:56 +0000

    withoutComments done, recursively

---
+++ withoutComments/WithoutComments.cpp
@@ -0,0 +1,78 @@
+#include<iostream>
+#include<fstream>
+
+using namespace std;
+
+
+void recursive(char ch, istream& in, ostream& out, bool ignoring);
+
+int main() {
+
+  cout << "Testing: " << 16/2 << " = " << 4*2 << ".\n\n";
+
+  ifstream in;
+  ofstream out;
+  char ch;
+  bool ignoring = false;
+
+  in.open("ex.cpp");
+  out.open("WithoutComments.cpp");
+
+  if (in.fail()) {
+    cerr << "could not open ex.cpp" << endl;
+    return -1;
+  }
+
+  if (out.fail()) {
+    cerr << "could not create WithoutComments.cpp" << endl;
+    return -2;
+  }
+
+  recursive(ch, in, out, ignoring);
+
+  return 0;
+}
+
+
+void recursive(char ch, istream& in, ostream& out, bool ignoring) {
+  ch = in.get(); 
+
+  if (in.eof())
+    return;
+
+  if (!ignoring && ch != '/') {
+    out.put(ch);
+    return recursive(ch, in, out, false);
+  }
+
+  if (!ignoring && ch == '/') {
+    ch = in.get();
+
+    if (ch == '*')
+      return recursive(ch, in, out, true);
+
+    if (ch != '*') {
+      out.put('/');
+      out.put(ch);
+      return recursive(ch, in, out, false);
+    }
+  }
+
+  if (ignoring && ch != '*')
+    return recursive(ch, in, out, true);
+
+  if (ignoring && ch == '*'){
+    ch = in.get();
+
+    if (ch == '/') 
+      return recursive(ch, in, out, false);
+
+    if (ch != '/') 
+      return recursive(ch, in, out, true);
+  }
+
+  cerr << "error!" << endl;
+
+}
+
+
