83adef4572ea9b4b1b309d6cc584b194f5f1c20b
alex <adalyac@gmail.com>
2014-01-13 11:39:13 +0000

    awesome cheat sheet

---
+++ check.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+//#include <cstdlib>
+//#include <cstdio>
+
+int main ()
+{
+  char str [10];
+  int n = 109073;// index = 0;
+  sprintf (str, "%d", n); 
+  std::cout << str << std::endl;
+  return 0;
+}
